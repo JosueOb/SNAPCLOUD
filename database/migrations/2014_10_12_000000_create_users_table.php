2573cd23d8fd22c6268614b8b791f8cccfc983f1
JosueOb <josuericardocando@hotmail.com>
2019-05-23 03:09:11 -0500

    Ejecutando migraciÃones User y PasswordReset

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -14,13 +14,16 @@ class CreateUsersTable extends Migration
     public function up()
     {
         Schema::create('users', function (Blueprint $table) {
-            $table->bigIncrements('id');
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->rememberToken();
-            $table->timestamps();
+            $table->bigIncrements('id');//incremento automático sin signo (primary key)
+            $table->string('name',50);//varchar con una longitud opcional
+            $table->string('email')->unique();// varchar y se le agrega un índice único
+            $table->string('avatar')->nullable();
+            $table->string('state')->nullable();
+            $table->string('username',50)->unique();
+            $table->timestamp('email_verified_at')->nullable();//equivale a una columna TIMESTAMP y se acepta valores null
+            $table->string('password');//varchar con una longitud por defecto de 255
+            $table->rememberToken();//equivalente a la columna remember_token VARCHAR(100) que acepta valores null
+            $table->timestamps();//agrega la columna created_at y updated_at de tipo TIMESTAMP y que acepta valores null
         });
     }
 
