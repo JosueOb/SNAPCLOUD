682802736de01ceee31c1fe69188dbe82ebcc6c2
JosueOb <josuericardocando@hotmail.com>
2019-05-27 00:19:33 -0500

    Enviar correo de verificación de correo electrónico

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -45,8 +45,11 @@
                                     <input class="form-check-input" type="checkbox" name="remember" id="remember" {{ old('remember') ? 'checked' : '' }}>
 
                                     <label class="form-check-label" for="remember">
-                                        {{ __('Remember Me') }}
+                                        Recuérdarmelo
                                     </label>
+                                    {{-- <label class="form-check-label" for="remember">
+                                        {{ __('Remember Me') }}
+                                    </label> --}}
                                 </div>
                             </div>
                         </div>
@@ -54,13 +57,19 @@
                         <div class="form-group row mb-0">
                             <div class="col-md-8 offset-md-4">
                                 <button type="submit" class="btn btn-primary">
-                                    {{ __('Login') }}
+                                    Ingresar
                                 </button>
+                                {{-- <button type="submit" class="btn btn-primary">
+                                    {{ __('Login') }}
+                                </button> --}}
 
                                 @if (Route::has('password.request'))
                                     <a class="btn btn-link" href="{{ route('password.request') }}">
-                                        {{ __('Forgot Your Password?') }}
+                                        ¿Olvide mi contraseña?
                                     </a>
+                                    {{-- <a class="btn btn-link" href="{{ route('password.request') }}">
+                                        {{ __('Forgot Your Password?') }}
+                                    </a> --}}
                                 @endif
                             </div>
                         </div>
