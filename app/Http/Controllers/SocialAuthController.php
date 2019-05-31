84dc4d92116d5f81098873905936c99c0cab6af0
JosueOb <josuericardocando@hotmail.com>
2019-05-31 02:07:23 -0500

    Login con facebook

--- app/Http/Controllers/SocialAuthController.php
+++ app/Http/Controllers/SocialAuthController.php
@@ -15,6 +15,20 @@ class SocialAuthController extends Controller
     public function facebookCallback(){
         $userFaceboock = Socialite::driver('facebook')->user();
         // dd($user);
+
+        //Se verifica si el usuario con esta red socal existe
+        $existing = User::whereIn('id', function($query) use ($userFaceboock){
+            $query->from('social_profiles')
+            ->select('social_profiles.user_id')
+            ->where('social_profiles.social_id', $userFaceboock->id);
+        })->first();
+
+        if($existing !== null){
+            auth()->login($existing);
+            
+            return redirect('/home');
+        }
+
         $user = User::create([
             'name' => $userFaceboock->name,
             'email' => $userFaceboock->email,
@@ -26,7 +40,8 @@ class SocialAuthController extends Controller
             'social_id'=> $userFaceboock->id,
             'user_id' => $user->id,
         ]);
-
+        //se registra al usuario y a su vez se marca que el correo ha sido verificado
+        $user->markEmailAsVerified();
         //se realiza un login del usuario que se acabde crear
         auth()->login($user);
         return redirect('/home');
